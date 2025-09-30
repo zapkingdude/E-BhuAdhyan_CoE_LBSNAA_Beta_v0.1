ce9073d2d2624abd9d74c311cac8a6ce08d00cfc
zapkingdude <prasannaloves2b@gmail.com>
2025-09-30 14:30:06 +0530

    New video player modules pages and its resources are added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,137 @@
+//Global Var
+
+const videoTutorialsModsListData = {
+    "MainModuleList": [
+        {
+            "sno": 1,
+            "module_id": "md1",
+            "module_name": "NAKSHA - A Comprehensive Collection of Tutorial Sessions",
+            "module_description": "A collection of videos where several resource persons from various departments coming together to explain all needed topics for the benefit of NAKSHA Programme - From Overview of NAKHSA to the Technical Explanation of topics",
+            "module_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+            "VideoModulesList": [
+                {
+                    "sno": "md1_1",
+                    "vid_title": "Introduction to NAKSHA #1",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_Overview_thumbnail.jpg",
+                    "vid_src": "https://drive.google.com/file/d/1ILogZj1BRHCxIADcChWIdc7FZe6NyrVu/preview"
+                },{
+                    "sno": "md1_2",
+                    "vid_title": "Methodologies of aerial data acquisition",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1UxSaHDGtW-Jw8qJjOnS7UCpJAVe62KmV/preview"
+                },{
+                    "sno": "md1_3",
+                    "vid_title": "Introduction to GNSS rover",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1_M2HmvW-w8FldeEbP74v6HfulvBnXxwH/preview"
+                },{
+                    "sno": "md1_4",
+                    "vid_title": "Basic concepts of surveying Part 1",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1X9m2PYY4R9rCbRyAyBrRGa3LyINbAJPl/preview"
+                },{
+                    "sno": "md1_5",
+                    "vid_title": "Basic concepts of surveying Part 2",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/17PCm61ifeMFSnu7ABF_55Y2Ozoq68si8/preview"
+                },{
+                    "sno": "md1_6",
+                    "vid_title": "Demo for CORS Data Integration",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1Edt3Ppo4rCfBiIzY9Bk5EJcBDotszdiT/preview"
+                },{
+                    "sno": "md1_7",
+                    "vid_title": "Fundamentals of GIS",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1HGjZ4Oft_2LkVsBtpCM8cjd6xguzLRUE/preview"
+                },{
+                    "sno": "md1_8",
+                    "vid_title": "Drone instrument & payloads",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1ffZEr5-0avwUTCCVa4qeZpJ306dRzZOh/preview"
+                },{
+                    "sno": "md1_9",
+                    "vid_title": "Field Work Preparation",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1K2gB9NpaLpAUt12sxAx0t0nq2bDHERZz/preview"
+                },{
+                    "sno": "md1_10",
+                    "vid_title": "Ground Survey using GNSS rovers with CORS",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1o6EIQkH35o2qx9SlrUObUif3RENraYIF/preview"
+                },{
+                    "sno": "md1_11",
+                    "vid_title": "Ground survey using ETS",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1_E9uYvrmvJdPEm7aOyezeQh-0ccMinte/preview"
+                },{
+                    "sno": "md1_12",
+                    "vid_title": "Ground survey using MPSEDC app",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1owf8J1rB1n7ndCoARsksaS9xGXClJwKx/preview"
+                },{
+                    "sno": "md1_13",
+                    "vid_title": "Preparation & finalization of land parcels",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/13t3ujXH5Ji9aQq_A3P0MosFWxUGSTTMt/preview"
+                },{
+                    "sno": "md1_14",
+                    "vid_title": "Fundamentals of GIS",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1HGjZ4Oft_2LkVsBtpCM8cjd6xguzLRUE/preview"
+                },{
+                    "sno": "md1_15",
+                    "vid_title": "Objections and conflict resolution under NAKSHA",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/12lIRXBiIiM7USD2zWvNRV07DA8yRAQB_/preview"
+                },{
+                    "sno": "md1_16",
+                    "vid_title": "Digital land survey Kerala's experience",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1XSlDmw38vKzbQFC0jqjm3pRfk2jxm8a4/preview"
+                },{
+                    "sno": "md1_17",
+                    "vid_title": "Legal & administrative procedures involving objections & conflict resolution in AP",
+                    "vid_description": "",
+                    "vid_thumbnail_src": "../img/NAKSHA_thumbnail.png",
+                    "vid_src": "https://drive.google.com/file/d/1c-_I879uvGRl-ix5JLgG0YxMcvGHft5V/preview"
+                }
+            ]
+        },
+        {
+            "sno": 2,
+            "module_id": "md2",
+            "module_name": "NAKSHA - Extensive Tutorial",
+            "module_description": "A well curated course where every technological tools used and utilzied for the NAKSHA programme is explained for the benefits for people who look to understand about these concepts in detail",
+            "module_thumbnail_src": "../img/basics_GIS_Banner.jpg"
+        },
+        {
+            "sno": 3,
+            "module_id": "md3",
+            "module_name": "Basics of QGIS",
+            "module_description": "A basic understanding of QGIS, how to install it,  its components and tools",
+            "module_thumbnail_src": "../img/Basics_QGIS.png"
+        }
+    ]    
+};
+
+
 (function ($) {
     "use strict";
 
@@ -14,6 +148,10 @@
     
     // Initiate the wowjs
     new WOW().init();
+
+    //Initiate tooltip for bootstrap
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
     // Sticky Navbar
@@ -109,6 +247,14 @@
                 items:3
             }
         }
+    });
+
+
+    //Other event functions
+    //Index event functions
+    $("div.videoTutorialAnchor").on("click", function(e){
+        console.log(e);
+        window.location.href = 'videoTutorials.html';
     });
 
     
